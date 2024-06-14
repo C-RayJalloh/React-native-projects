0426544d6a5d47172447827210a5a8a8cc678173
“Jahrulo” <mserayjay@gmail.com>
2024-06-14 10:36:24 +0000

    "Deleted App.tsx and updated index.js to import App from ./src/App"

---
+++ RollDice/rollDiceGame/src/App.tsx
@@ -0,0 +1,58 @@
+
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+     
+      <View>
+        <View >
+        <Text>Hello worldly world</Text>
+        </View>
+      </View>
+
+  
+  
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+});
+
+export default App;
