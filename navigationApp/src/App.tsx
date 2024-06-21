27febdfc1a64ee1b50d98cb5ef1d12e11fb7a538
“Jahrulo” <mserayjay@gmail.com>
2024-06-21 16:36:03 +0000

    "Added @react-navigation/native-stack dependency and implemented navigation with NavigationContainer and createStackNavigator in App.tsx"

--- navigationApp/src/App.tsx
+++ navigationApp/src/App.tsx
@@ -4,32 +4,35 @@ import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
 
+// NAVIGATION
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+// SCREENS
+import Home from '../src/screens/Home'
+import Details from './screens/Details'
 
+// Navigation configuration
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
-
-
   return (
-    <SafeAreaView>
-      <View>
-         <Text>Testing</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
     );
 }
 
 const styles = StyleSheet.create({
-
 });
 
 export default App;
