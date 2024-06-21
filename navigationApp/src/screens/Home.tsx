dd2f9acd83e7cac19411e6615897abe62929b003
“Jahrulo” <mserayjay@gmail.com>
2024-06-21 17:43:57 +0000

    "Added styles, button, and navigation to Home screen"

--- navigationApp/src/screens/Home.tsx
+++ navigationApp/src/screens/Home.tsx
@@ -1,13 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 import React from 'react'
 
+// NAVIGATION for screen props
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
-export default function Home() {
+import {RootStackParamList} from '../App'
+
+// setting up type safety
+ type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+
+export default function Home({navigation}: HomeScreenProps) {
   return (
-    
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+      
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details', {ProductId: '60'})}
+      />
       
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFF2F2',
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#000000'
+    }
+})
