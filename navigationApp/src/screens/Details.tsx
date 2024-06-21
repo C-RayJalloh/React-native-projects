bb9411d4747934d5d6624331d045620ef1e7aa83
“Jahrulo” <mserayjay@gmail.com>
2024-06-21 18:22:52 +0000

    "Updated Details.tsx screen to include styles, button, and navigation props"

--- navigationApp/src/screens/Details.tsx
+++ navigationApp/src/screens/Details.tsx
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 import React from 'react'
 
-export default function Details() {
+// NAVIGATION for screen props
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+import {RootStackParamList} from '../App'
+
+type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>
+export default function Details({navigation}: DetailsProps) {
   return (
-    <View>
-      <Text>Details</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Details Page</Text>
+      <Text style={styles.text}>{}</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFF2F2',
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#000000'
+    }
+})
