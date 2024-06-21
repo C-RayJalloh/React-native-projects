87996519cdbf190e17901a7ed4a9bdf4f26579ec
“Jahrulo” <mserayjay@gmail.com>
2024-06-21 18:42:07 +0000

    "Formatted code in Details.tsx, added btn style to stylesheet"

---
+++ navigationApp/src v1/App.tsx
@@ -0,0 +1,50 @@
+
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+// NAVIGATION
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+// SCREENS
+import Home from '../src/screens/Home'
+import Details from './screens/Details'
+
+
+// propTypes
+export type RootStackParamList = {
+  Home: undefined;
+ Details: { ProductId: string }
+
+};
+
+// Navigation configuration
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function App(): React.JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={Home} options={{
+          title: 'Homepage'
+        }} />
+        <Stack.Screen name="Details" component={Details} options={{
+          title: 'Details page'
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
---
+++ navigationApp/src v1/screens/Home.tsx
@@ -0,0 +1,39 @@
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React from 'react'
+
+// NAVIGATION for screen props
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+import {RootStackParamList} from '../App'
+
+// setting up type safety
+ type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+
+export default function Home({navigation}: HomeScreenProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+    {/* navigating from home page to details */}
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details', {ProductId: '60'})}
+      />
+
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFF2F2',
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#000000'
+    }
+})
